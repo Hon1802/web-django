9b2e9bad625347caff1ca974de6e8b93c6edbe71
Nguyễn Văn Hơn <74673759+Hon1802@users.noreply.github.com>
2023-10-22 14:44:36 +0700

    .
    .

--- laptopSite/settings.py
+++ laptopSite/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'shop-shoes-hubg.onrender.com']
 # Application definition
 INSTALLED_APPS = [
     "CustomerSite",
